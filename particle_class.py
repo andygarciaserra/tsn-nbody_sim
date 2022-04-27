5b17b2c12b18730f120fbb764acab7c196a9c584
andygarciaserra <alu0101451923@ull.edu.es>
2022-04-27 17:22:05 +0100

    Progress on particle class and universe generation

---
+++ particle_class.py
@@ -0,0 +1,37 @@
+# Particle class for N-body simulation:
+
+class particle:
+    xpos = 0
+    ypos = 0
+    vx = 0
+    vy = 0
+    
+    def __init__(self,x,y,xvel,yvel):
+        self.xpos = x
+        self.ypos = y
+        self.vx = xvel
+        self.vy = yvel
+        
+    def pos(self):
+        return np.array([xpos,ypos])
+    def vel(self):
+        return np.array([vx,vy])
+
+    
+
+class universe:
+    particles = np.array([])
+    
+    def __init__(self,N,Lx,Ly):
+        for i in range(N):
+            x = np.random.randint(Lx)
+            y = np.random.randint(Ly)
+            xvel = np.random.uniform(-1,1)
+            yvel = sqrt(1-xvel**2)
+            part = particle(x,y,xvel,yvel)
+            self.particles = np.concatenate(particles,part)
+        
+    def add(self,part):
+    particles = part.append(part)
+    def plot(self):
+        plt.plot()
